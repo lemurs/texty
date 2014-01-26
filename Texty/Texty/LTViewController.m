02a975449865bc5112f431575b2fa94fed935fea
Mike Lee <bmf.home@me.com>
2014-01-26 21:39:28 +0100

    Add a text view and set it up.

--- Texty/Texty/LTViewController.m
+++ Texty/Texty/LTViewController.m
@@ -1,6 +1,6 @@
 #import "LTViewController.h"
 
-@interface LTViewController ()
+@interface LTViewController () <UITextViewDelegate>
 
 @end
 
@@ -12,7 +12,31 @@
 {
     UIView *view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     view.backgroundColor = [UIColor whiteColor];
+
+    static const CGFloat givenMaximumFontSize = 50.0;
+
+    UITextView *textView = [[UITextView alloc] initWithFrame:view.bounds textContainer:nil];
+    textView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+    textView.delegate = self;
+    textView.font = [UIFont systemFontOfSize:givenMaximumFontSize];
+    textView.returnKeyType = UIReturnKeyDone;
+    textView.text = NSLocalizedString(@"Hello, World!", @"Traditional greeting");
+    textView.textAlignment = NSTextAlignmentCenter;
+    [view addSubview:textView];
+
     self.view = view;
+}
+
+
+#pragma mark - UITextViewDelegate
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text;
+{
+    if ([text rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]].location == NSNotFound)
+        return YES;
+
+    [textView resignFirstResponder];
+    return NO;
 }
 
 @end
